60a0f27bcfde6253abc4ba0da0a38dfd906adaee
Rodrigolega <15068@ulsachihuahua.edu.mx>
2025-02-05 10:25:22 -0600

    se corrigieron archivos para entregarse

--- piedrapap.java
+++ piedrapap.java
@@ -1,10 +1,17 @@
-import java.util.Random;
+/*
+ * imprimir aleatoriamente piedra papel o tijera
+ * Autor: Rodrigo Legarreta Soto
+ * matricula: 15068
+ * dia: 05/02/2025
+ */
 
+import java.util.Random;
+// se imprime el paquete para que el random funcione
 class Piedrapap {
     public void startGame() {
         String[] opciones = {"piedra", "papel", "tijera"};
         Random random = new Random();
-        
+        //se usa la funcion random para que elija entre piedra papel o tijera
         int indice = random.nextInt(opciones.length);
         
         System.out.println(opciones[indice]);
--- promedio.java
+++ promedio.java
@@ -1,8 +1,15 @@
+/*
+ * imprimir lista de companeros de clase
+ * Autor: Rodrigo Legarreta Soto
+ * matricula: 15068
+ * dia: 05/02/2025
+ */
+
 public class Promedio {
     public void prom() { 
         double[] calificaciones = {8.5, 9.2, 7.3}; 
         double suma = 0;
-
+// se usa doble para hacer la suma y poder sacar el promedio despues dividiendolo por el tamano de la lista
         for (int i = 0; i < calificaciones.length; i++) {
             suma += calificaciones[i];
         }
